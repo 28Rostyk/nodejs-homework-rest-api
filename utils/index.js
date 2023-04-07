6f4118c7304c6fa7093633971f62889588e9d3d5
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-04-07 18:22:49 +0300

    complete homework-3

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+const ctrlWrapper = require("./ctrlWrapper");
+const validateBody = require("./validateBody");
+const handleMongooseError = require("./handleMangooseError");
+const isValidId = require("./isValidid");
+
+module.exports = {
+  ctrlWrapper,
+  validateBody,
+  handleMongooseError,
+  isValidId,
+};
